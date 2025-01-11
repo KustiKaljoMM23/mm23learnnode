70db580e9c7373b3be653fef27d18102fa362204
Kusti Kaljo <kusti.kaljo@tptlive.ee>
2025-01-12 01:03:51 +0200

    Change webpack to use imports, add fetch data example

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import autoprefixer from 'autoprefixer';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+let response = await fetch('https://api.spaceflightnewsapi.net/v4/articles/?format=json&limit=12');
+let body = await response.json();
+let articles = body.results;
+
+export default {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
@@ -27,22 +35,20 @@ module.exports = {
                 use: [
                     {
                         // Adds CSS to the DOM by injecting a '<style>' tag
-                        loader: 'style-loader',
+                        loader: "style-loader",
                     },
                     {
                         // Interprets '@import' and 'url()' like 'import/require()'
-                        loader: 'css-loader',
+                        loader: "css-loader",
                     },
                     {
                         // Loader for webpack to process CSS with PostCSS
-                        loader: 'postcss-loader',
+                        loader: "postcss-loader",
                         options: {
                             postcssOptions: {
-                                plugins: [
-                                    autoprefixer
-                                ]
-                            }
-                        }
+                                plugins: [autoprefixer]
+                            },
+                        },
                     },
                     {
                         // Loads a SASS/SCSS file and compiles it to CSS
@@ -77,6 +83,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'blog.html',
             template: './src/blog.njk',
+            templateParameters: {
+                articles: articles
+            }
         }),
         new HtmlWebpackPlugin({
             filename: 'contact.html',
