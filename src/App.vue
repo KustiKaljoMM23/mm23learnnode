900241f1d067dc0a638dcc8c3d49d2255dff4e09
Kusti Kaljo <kusti.kaljo@tptlive.ee>
2025-01-18 02:53:10 +0200

    Finish button component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Btn from './components/Btn.vue';
 </script>
 <template>
     <div class="container mt-2">
-        <Btn color="primary" light>Button is cool</Btn>
+        <Btn color="primary" dark size="large" rounded state="loading">Button is cool</Btn>
+        <button class="button is-loading">Test button</button>
     </div>
 </template>
