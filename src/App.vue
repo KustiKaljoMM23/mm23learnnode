8f88e1417bfb744f7d598f4af16273487f201967
Kusti Kaljo <kusti.kaljo@tptlive.ee>
2025-01-18 02:56:15 +0200

    Move buttons code to separate page file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup>
-import Btn from './components/Btn.vue';
+
 </script>
 <template>
-    <div class="container mt-2">
-        <Btn color="primary" dark size="large" rounded state="loading">Button is cool</Btn>
-        <button class="button is-loading">Test button</button>
-    </div>
+    
 </template>
