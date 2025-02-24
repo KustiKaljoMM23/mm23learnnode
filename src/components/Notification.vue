7e10d99abed8c7f2173b1f96d3bde038d52bef15
Kusti Kaljo <kusti.kaljo@tptlive.ee>
2025-02-24 23:40:22 +0200

    fixed notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue';
+let props = defineProps({
+color: {
+        type: String,
+        required: false,
+        validator(value, props) {
+            return [
+                'primary',
+                'link',
+                'info',
+                'success',
+                'warning',
+                'danger',
+                'white',
+                'light',
+                'dark',
+                'text',
+                'ghost',
+            ].includes(value);
+        }
+    },
+});
+
+</script>
+<template>
+    <button class="notification" :class="classNames">
+        <slot></slot>
+    </button>
+</template>
